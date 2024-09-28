c61a44c492c22892b9f6a9bb3b05412f47527da8
Mattia <82611659+Xhst@users.noreply.github.com>
2024-09-28 20:27:49 +0200

    chore: print window created

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "core/base.hpp"
 #include "core/window.hpp"
 
+#include <iostream>
+
 int main() {
     auto window = CGEngine::Window::create();
+
+    std::cout << "Window created: " << window->getWidth() << "x" << window->getHeight() << std::endl;
 
     while (!window->shouldClose()) {
         glfwPollEvents();
