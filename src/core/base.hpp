20f53f58ac7fe3c180ff2057b352188cc8727e7e
Mattia <82611659+Xhst@users.noreply.github.com>
2024-09-28 20:00:44 +0200

    feat: base

---
+++ src/core/base.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+namespace CGEngine {
+
+    template<typename T>
+    using Unique = std::unique_ptr<T>;
+
+    template<typename T, typename ... Args>
+    constexpr Unique<T> createUnique(Args&& ... args) {
+        return std::make_unique<T>(std::forward<Args>(args)...);
+    }
+
+    template<typename T>
+    using Shared = std::shared_ptr<T>;
+
+    template<typename T, typename ... Args>
+    constexpr Shared<T> createShared(Args&& ... args) {
+        return std::make_shared<T>(std::forward<Args>(args)...);
+    }
+
+}
