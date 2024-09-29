ab1a28fd40a2bc32cbfe2b90f2124ebcbacc7cc7
Prox747 <carloproserpio1@gmail.com>
2024-09-29 20:54:48 +0200

    feat: device init vk

--- src/engine.hpp
+++ src/engine.hpp
@@ -3,20 +3,23 @@
 #include "core/memory.hpp"
 #include "core/window.hpp"
 #include "renderer/pipeline.hpp"
+#include "device.hpp"
 
 namespace CGEngine {
 
     class Engine {
     public:
-        Engine();
-        ~Engine() = default;
-
         void run();
 
     private:
         bool isRunning();
 
-        Unique<Window> m_window;
-        Pipeline pipeline{"../shaders/simple_shader.vert.spv", "../shaders/simple_shader.frag.spv"};
+        Window m_window{WindowData()};
+        Device m_device{m_window};
+        Pipeline pipeline{
+            m_device,
+            "../shaders/simple_shader.vert.spv",
+            "../shaders/simple_shader.frag.spv",
+            Pipeline::defaultPipelineConfigInfo(m_window.getWidth(), m_window.getHeight())};
     };
 }
