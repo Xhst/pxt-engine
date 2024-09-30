48a20bdcfd5164aea02d0c0c5c9c167ea6b5b86e
Mattia <82611659+Xhst@users.noreply.github.com>
2024-09-30 15:33:10 +0200

    feat: creation of command buffers and frames
    Co-Authored-By: Carlo Proserpio <115183769+Prox747@users.noreply.github.com>

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,17 +1,123 @@
 #include "engine.hpp"
+#include "core/memory.hpp"
 
 #include <iostream>
+#include <array>
+#include <stdexcept>
+#include <cassert>
 
 namespace CGEngine {
+
+    Engine::Engine() {
+        createPipelineLayout();
+        createPipeline();
+        createCommandBuffers();
+    }
+
+    Engine::~Engine() {
+        vkDestroyPipelineLayout(m_device.device(), m_pipelineLayout, nullptr);
+    }
 
     void Engine::run() {
         while (isRunning()) {
             glfwPollEvents();
+            drawFrame();
         }
+
+        vkDeviceWaitIdle(m_device.device());
     }
 
     bool Engine::isRunning() {
         return !m_window.shouldClose();
+    }
+
+    void Engine::createPipelineLayout() {
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+        pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipelineLayoutInfo.setLayoutCount = 0;
+        pipelineLayoutInfo.pSetLayouts = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 0;
+        pipelineLayoutInfo.pPushConstantRanges = 0;
+
+        if (vkCreatePipelineLayout(m_device.device(), &pipelineLayoutInfo, nullptr, &m_pipelineLayout) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create pipeline layout!");
+        }
+    }
+
+    void Engine::createPipeline() {
+        auto pipelineConfig = Pipeline::defaultPipelineConfigInfo(m_swapChain.width(), m_swapChain.height());
+        pipelineConfig.renderPass = m_swapChain.getRenderPass();
+        pipelineConfig.pipelineLayout = m_pipelineLayout;
+
+        m_pipeline = createUnique<Pipeline>(
+            m_device,
+            "../shaders/simple_shader.vert.spv",
+            "../shaders/simple_shader.frag.spv",
+            pipelineConfig
+        );
+    }
+
+    void Engine::createCommandBuffers() {
+        m_commandBuffers.resize(m_swapChain.imageCount());
+
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandPool = m_device.getCommandPool();
+        allocInfo.commandBufferCount = static_cast<uint32_t>(m_commandBuffers.size());
+
+        if (vkAllocateCommandBuffers(m_device.device(), &allocInfo, m_commandBuffers.data()) != VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate command buffers!");
+        }
+
+        for (size_t i = 0; i < m_commandBuffers.size(); i++) {
+            VkCommandBufferBeginInfo beginInfo{};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+            if (vkBeginCommandBuffer(m_commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+                throw std::runtime_error("failed to begin recording command buffer!");
+            }
+
+            VkRenderPassBeginInfo renderPassInfo{};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = m_swapChain.getRenderPass();
+            renderPassInfo.framebuffer = m_swapChain.getFrameBuffer(i);
+
+            renderPassInfo.renderArea.offset = {0, 0};
+            renderPassInfo.renderArea.extent = m_swapChain.getSwapChainExtent();
+
+            std::array<VkClearValue, 2> clearValues{};
+            clearValues[0].color = {0.1f, 0.5f, 0.02f, 1.0f};
+            clearValues[1].depthStencil = {1.0f, 0};
+
+            renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+            renderPassInfo.pClearValues = clearValues.data();
+
+            vkCmdBeginRenderPass(m_commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+            
+            m_pipeline->bind(m_commandBuffers[i]);
+            vkCmdDraw(m_commandBuffers[i], 3, 1, 0, 0);
+            
+            vkCmdEndRenderPass(m_commandBuffers[i]);
+
+            if (vkEndCommandBuffer(m_commandBuffers[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to record command buffer!");
+            }
+        }
+    }
+
+    void Engine::drawFrame() {
+        uint32_t imageIndex;
+        VkResult result = m_swapChain.acquireNextImage(&imageIndex);
+
+        if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+            throw std::runtime_error("failed to acquire swap chain image!");
+        }
+
+        result = m_swapChain.submitCommandBuffers(&m_commandBuffers[imageIndex], &imageIndex);
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to present swap chain image!");
+        }
     }
 
 }
--- src/engine.hpp
+++ src/engine.hpp
@@ -3,23 +3,34 @@
 #include "core/memory.hpp"
 #include "core/window.hpp"
 #include "renderer/pipeline.hpp"
+#include "renderer/swap_chain.hpp"
 #include "device.hpp"
+
 
 namespace CGEngine {
 
     class Engine {
     public:
+        Engine();
+        ~Engine();
+
+        Engine(const Engine&) = delete;
+        Engine& operator=(const Engine&) = delete;
+        
         void run();
 
     private:
         bool isRunning();
+        void createPipelineLayout();
+        void createPipeline();  
+        void createCommandBuffers();
+        void drawFrame();
 
         Window m_window{WindowData()};
         Device m_device{m_window};
-        Pipeline pipeline{
-            m_device,
-            "../shaders/simple_shader.vert.spv",
-            "../shaders/simple_shader.frag.spv",
-            Pipeline::defaultPipelineConfigInfo(m_window.getWidth(), m_window.getHeight())};
+        SwapChain m_swapChain{m_device, m_window.getExtent()};
+        Unique<Pipeline> m_pipeline;
+        VkPipelineLayout m_pipelineLayout;
+        std::vector<VkCommandBuffer> m_commandBuffers;
     };
 }
