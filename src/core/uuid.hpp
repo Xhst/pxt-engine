3389568001b6c39ee11313585d6773d2a8285f29
Mattia <82611659+Xhst@users.noreply.github.com>
2024-10-02 15:54:50 +0200

    feat: uuid

---
+++ src/core/uuid.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <string>
+
+namespace CGEngine {
+
+    enum UUIDVersion {
+        V4,
+        V7
+    };
+
+    class UUID {
+    public:
+        UUID();
+        UUID(UUIDVersion version);
+        UUID(const std::string& uuid);
+        UUID(const UUID&) = default;
+
+        operator size_t() const {
+            return std::hash<std::string>{}(m_uuid);
+        }
+
+
+    private:
+        std::string m_uuid;
+
+        static std::string generateUUIDv4();
+        static std::string generateUUIDv7();
+        
+    };
+}
+
+namespace std {
+
+	template <typename T> struct hash;
+
+	template<>
+	struct hash<CGEngine::UUID>
+	{
+		std::size_t operator()(const CGEngine::UUID& uuid) const
+		{
+			return (size_t) uuid;
+		}
+	};
+
+}
