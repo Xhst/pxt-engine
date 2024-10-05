8241579ed15cacb06a7b6ddbb34c04d4a08d5fdc
Mattia <82611659+Xhst@users.noreply.github.com>
2024-10-05 08:06:21 +0200

    chore: components cast

--- src/scene/ecs/component.hpp
+++ src/scene/ecs/component.hpp
@@ -14,6 +14,9 @@ namespace CGEngine
 
         IDComponent() = default;
         IDComponent(const IDComponent&) = default;
+
+        operator UUID&() { return uuid; }
+        operator const UUID&() const { return uuid; }
     };
 
     struct NameComponent {
@@ -22,6 +25,9 @@ namespace CGEngine
         NameComponent() = default;
         NameComponent(const NameComponent&) = default;
         NameComponent(const std::string& name) : name(name) {}
+
+        operator std::string&() { return name; }
+        operator const std::string&() const { return name; }
     };
 
     struct ColorComponent {
@@ -31,6 +37,10 @@ namespace CGEngine
         ColorComponent(const ColorComponent&) = default;
         ColorComponent(const glm::vec4& color) : color(color) {}
         ColorComponent(const glm::vec3& color) : color(glm::vec4{color, 1.f}) {}
+
+        operator glm::vec4&() { return color; }
+        operator const glm::vec4&() const { return color; }
+        operator glm::vec3() const { return glm::vec3(color); }
     };
 
     struct Transform2dComponent {
@@ -38,20 +48,28 @@ namespace CGEngine
         glm::vec2 scale{1.f, 1.f};
         float rotation;
 
-        glm::mat2 mat2 () {
+        glm::mat2 mat2() {
             const float sin = glm::sin(rotation);
             const float cos = glm::cos(rotation);
             glm::mat2 rotationMatrix{{cos, sin}, {-sin, cos}};
 
             glm::mat2 scaleMatrix{{scale.x, 0.f}, {0.f, scale.y}};
+            
             return rotationMatrix * scaleMatrix;
         }
 
         Transform2dComponent() = default;
         Transform2dComponent(const Transform2dComponent&) = default;
-        Transform2dComponent(const glm::vec2& translation) : translation(translation) {}
-        Transform2dComponent(const glm::vec2& translation, const glm::vec2& scale) : translation(translation), scale(scale) {}
-        Transform2dComponent(const glm::vec2& translation, const glm::vec2& scale, float rotation) : translation(translation), scale(scale), rotation(rotation) {}
+        Transform2dComponent(const glm::vec2& translation) 
+            : translation(translation) {}
+
+        Transform2dComponent(const glm::vec2& translation, const glm::vec2& scale) 
+            : translation(translation), scale(scale) {}
+
+        Transform2dComponent(const glm::vec2& translation, const glm::vec2& scale, float rotation) 
+            : translation(translation), scale(scale), rotation(rotation) {}
+    
+        operator glm::mat2() { return mat2(); }
     };
 
     struct ModelComponent {
@@ -60,5 +78,8 @@ namespace CGEngine
         ModelComponent() = default;
         ModelComponent(const ModelComponent&) = default;
         ModelComponent(const Shared<Model>& model) : model(model) {}
+
+        operator Shared<Model>&() { return model; }
+        operator const Shared<Model>&() const { return model; }
     };
 }
