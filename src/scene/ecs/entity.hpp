bed1c6fa3518fc6a7a78a3b89a7d62e1c09a61d9
Mattia <82611659+Xhst@users.noreply.github.com>
2024-10-06 18:19:23 +0200

    docs: entity docs

--- src/scene/ecs/entity.hpp
+++ src/scene/ecs/entity.hpp
@@ -15,37 +15,73 @@ namespace CGEngine {
         operator entt::entity() const { return m_enttEntity; }
         operator bool() const { return m_enttEntity != entt::null; }
 
-        template <typename... T>
+        /**
+         * @brief Check if entity has a component
+         * 
+         * @tparam T Component type
+         * @return true if entity has component, false otherwise
+         */
+        template <typename... Components>
         bool has() {
-            return m_scene->m_registry.all_of<T...>(m_enttEntity);
+            return m_scene->m_registry.all_of<Components...>(m_enttEntity);
         }
 
-        template <typename T>
-        T& get() {
+        /**
+         * @brief Get a component from entity
+         * 
+         * @tparam Component type
+         * @return Reference to component
+         */
+        template <typename Component>
+        Component& get() {
             assert(has<T>() && "Entity does not have component");
 
-            return m_scene->m_registry.get<T>(m_enttEntity);
+            return m_scene->m_registry.get<Component>(m_enttEntity);
         }
 
-        template <typename T, typename... Args>
+        /**
+         * @brief Add a component to entity
+         * 
+         * Self referential method to allow chaining of add calls
+         * 
+         * @tparam Component type
+         * @return Reference to entity
+         */
+        template <typename Component, typename... Args>
         Entity& add(Args&&... args) {
-            m_scene->m_registry.emplace<T>(m_enttEntity, std::forward<Args>(args)...);
+            m_scene->m_registry.emplace<Component>(m_enttEntity, std::forward<Args>(args)...);
             return *this;
         }
 
-        template <typename T, typename... Args>
-        T& addAndGet(Args&&... args) {
-            return m_scene->m_registry.emplace<T>(m_enttEntity, std::forward<Args>(args)...);
+        /**
+         * @brief Add a component to entity and return a reference to it
+         * 
+         * @tparam Component type
+         * @return Reference to component
+         */
+        template <typename Component, typename... Args>
+        Component& addAndGet(Args&&... args) {
+            return m_scene->m_registry.emplace<Component>(m_enttEntity, std::forward<Args>(args)...);
         }
 
-        template <typename T>
+        /**
+         * @brief Remove a component from entity
+         * 
+         * @tparam Component type
+         */
+        template <typename ComponentT>
         void remove() {
             static_assert(!std::is_same<T, IDComponent>::value, "Cannot remove ID component");
-            assert(has<T>() && "Entity does not have component");
+            assert(has<Component>() && "Entity does not have component");
 
-            m_scene->m_registry.remove<T>(m_enttEntity);
+            m_scene->m_registry.remove<Component>(m_enttEntity);
         }
 
+        /**
+         * @brief Get the UUID of the entity
+         * 
+         * @return UUID of the entity
+         */
         UUID getUUID() {
             return get<IDComponent>().uuid;
         }
