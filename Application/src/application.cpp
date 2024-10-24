26d6ec88d60bb036ae2778118386f35461ada3e3
Xhst <82611659+Xhst@users.noreply.github.com>
2024-10-24 17:41:32 +0200

    feat: added bunny

--- Application/src/application.cpp
+++ Application/src/application.cpp
@@ -23,11 +23,23 @@ public:
         
         camera.addAndGet<ScriptComponent>().bind<CameraController>();
 
-        Shared<Model> model = Model::createModelFromFile(getDevice(), "../assets/models/smooth_vase.obj");
-        Entity entity = getScene().createEntity("object")
-            .add<TransformComponent>(glm::vec3{0.0f, 0.0f, 2.5f}, glm::vec3{0.5f, 0.5f, .5f}, glm::vec3{0.0f, 0.0f, 0.0f})
+        Shared<Model> model_smooth_vase = Model::createModelFromFile(getDevice(), "../assets/models/smooth_vase.obj");
+        Entity entity = getScene().createEntity("smooth_vase")
+            .add<TransformComponent>(glm::vec3{-0.2f, 0.1f, 0.5f}, glm::vec3{0.5f, 0.25f, .5f}, glm::vec3{0.0f, 0.0f, 0.0f})
             .add<ColorComponent>(glm::vec3{0.6f, 0.0f, 0.5f})
-            .add<ModelComponent>(model);
+            .add<ModelComponent>(model_smooth_vase);
+
+        Shared<Model> model_bunny = Model::createModelFromFile(getDevice(), "../assets/models/stanford_bunny.obj");
+        entity = getScene().createEntity("bunny")
+            .add<TransformComponent>(glm::vec3{0.0f, 0.1f, 0.5f}, glm::vec3{0.5f, 0.5f, .5f}, glm::vec3{0.0f, glm::pi<float>(), 0.0f})
+            .add<ColorComponent>(glm::vec3{0.6f, 0.0f, 0.5f})
+            .add<ModelComponent>(model_bunny);
+
+        Shared<Model> model_flat_vase = Model::createModelFromFile(getDevice(), "../assets/models/flat_vase.obj");
+        entity = getScene().createEntity("flat_vase")
+            .add<TransformComponent>(glm::vec3{0.2f, 0.1f, 0.5f}, glm::vec3{0.5f, 0.25f, .5f}, glm::vec3{0.0f, 0.0f, 0.0f})
+            .add<ColorComponent>(glm::vec3{0.6f, 0.0f, 0.5f})
+            .add<ModelComponent>(model_flat_vase);
     }
 };
 
