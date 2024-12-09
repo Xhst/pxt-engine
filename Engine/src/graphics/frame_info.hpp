84fb6889897d33c6a4e4bdb491b0ba54468eec79
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2024-12-09 15:44:40 +0100

    feat: ubo and point light structs

--- Engine/src/graphics/frame_info.hpp
+++ Engine/src/graphics/frame_info.hpp
@@ -6,6 +6,22 @@
 #include <vulkan/vulkan.h>
 
 namespace CGEngine {
+
+    #define MAX_LIGHTS 10
+
+    struct PointLight {
+        glm::vec4 position{};
+        glm::vec4 color{}; // w is intensity
+    };
+
+    struct GlobalUbo {
+        glm::mat4 projection{1.f};
+        glm::mat4 view{1.f};
+        glm::vec4 ambientLightColor{1.f, 1.f, 1.f, .02f};
+        PointLight pointLights[MAX_LIGHTS];
+        int numLights;
+    };
+
     struct FrameInfo {
         int frameIndex;
         float frameTime;
