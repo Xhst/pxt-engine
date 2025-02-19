350eb8db422fd3506270a4a823949894b68cf347
CARLO PROSERPIO <115183769+Prox747@users.noreply.github.com>
2025-02-19 08:49:02 +0100

    feat: mouse camera movement

--- Application/src/camera_controller.hpp
+++ Application/src/camera_controller.hpp
@@ -9,4 +9,8 @@ class CameraController : public Script {
     private:
         float m_moveSpeed{1.f};
         float m_lookSpeed{1.75f};
+
+        glm::vec2 m_lastMousePos{0.f, 0.f};
+        bool m_firstMouse = true;
+        float m_mouseSensitivity = 0.0025f; // Adjust to taste
 };
