25a39fa9728d758bb5e5ae357ee59542e2bb6603
Xhst <82611659+Xhst@users.noreply.github.com>
2025-02-20 08:56:52 +0100

    chore: scene roof and wall

--- Application/src/application.cpp
+++ Application/src/application.cpp
@@ -30,8 +30,18 @@ public:
             .add<MaterialComponent>(glm::vec3{1.0f, 1.0f, 1.0f})
             .add<ModelComponent>(quad);
 
+        entity = getScene().createEntity("Roof")
+            .add<TransformComponent>(glm::vec3{0.f, -2.0f, 0.f}, glm::vec3{1.f, 1.f, 1.f}, glm::vec3{0.0f, 0.0f, 0.0f})
+            .add<MaterialComponent>(glm::vec3{1.0f, 1.0f, 1.0f})
+            .add<ModelComponent>(quad);
+
         entity = getScene().createEntity("BackWall")
             .add<TransformComponent>(glm::vec3{0.0f, -1.f, 1.f}, glm::vec3{1.f, 1.f, 1.f}, glm::vec3{glm::pi<float>()/2, 0.0f, 0.0f})
+            .add<MaterialComponent>(glm::vec3{1.0f, 1.0f, 1.0f})
+            .add<ModelComponent>(quad);
+
+            entity = getScene().createEntity("FrontWall")
+            .add<TransformComponent>(glm::vec3{0.0f, -1.f, -1.f}, glm::vec3{1.f, 1.f, 1.f}, glm::vec3{-glm::pi<float>()/2, 0.0f, 0.0f})
             .add<MaterialComponent>(glm::vec3{1.0f, 1.0f, 1.0f})
             .add<ModelComponent>(quad);
 
