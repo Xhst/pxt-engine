3200c1c58198d8c9bd19ac4b30f1fa6cd1c25c7d
Xhst <82611659+Xhst@users.noreply.github.com>
2025-03-24 20:30:31 +0100

    docs: input

--- Engine/src/core/input/input.hpp
+++ Engine/src/core/input/input.hpp
@@ -8,24 +8,60 @@
 
 namespace PXTEngine {
 
+    /**
+     * @class Input
+     * @brief Class for handling input events.
+     * 
+     * The Input class provides static methods for querying input events such as key presses, 
+     * mouse button presses, and mouse movement.
+     */
     class Input {
     public:
+        /**
+         * @brief Checks if a key is released.
+         * 
+         * @param key The key to check.
+         * @return True if the key is released, false otherwise.
+         */
         static bool isKeyReleased(KeyCode key) {
             return glfwGetKey(getWindow(), mapToGLFWKey(key)) == GLFW_RELEASE;
         }
         
+        /**
+         * @brief Checks if a key is currently pressed.
+         * 
+         * @param key The key to check.
+         * @return True if the key is currently pressed, false otherwise.
+         */
         static bool isKeyPressed(KeyCode key) {
             return glfwGetKey(getWindow(), mapToGLFWKey(key)) == GLFW_PRESS;
         }
 
+        /**
+         * @brief Checks if a key is being repeated (held down).
+         * 
+         * @param key The key to check.
+         * @return True if the key is being repeated, false otherwise.
+         */
         static bool isKeyRepeated(KeyCode key) {
             return glfwGetKey(getWindow(), mapToGLFWKey(key)) == GLFW_REPEAT;
         }
 
+        /**
+         * @brief Checks if a mouse button is pressed.
+         * 
+         * @param button The mouse button to check.
+         * @return True if the mouse button is pressed, false otherwise.
+         */
         static bool isMouseButtonPressed(MouseButton button) {
             return glfwGetMouseButton(getWindow(), mapToGLFWMouseButton(button)) == GLFW_PRESS;
         }
 
+        /**
+         * @brief Gets the current mouse position.
+         * 
+         * @return The current mouse position.
+         */
         static glm::vec2 getMousePosition() {
             double x, y;
 		    glfwGetCursorPos(getWindow(), &x, &y);
