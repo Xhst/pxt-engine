6a941ec7254ef65e7a52494b96815a272e4f71ca
Xhst <82611659+Xhst@users.noreply.github.com>
2025-03-27 20:24:35 +0100

    refactor: descriptor writer

--- Engine/src/graphics/descriptors/descriptor_writer.cpp
+++ Engine/src/graphics/descriptors/descriptor_writer.cpp
@@ -7,48 +7,6 @@ namespace PXTEngine {
     DescriptorWriter::DescriptorWriter(DescriptorSetLayout& setLayout, DescriptorPool& pool)
         : m_setLayout{setLayout}, m_pool{pool} {}
 
-    DescriptorWriter& DescriptorWriter::writeBuffer(
-            uint32_t binding, VkDescriptorBufferInfo *bufferInfo) {
-        assert(m_setLayout.m_bindings.count(binding) == 1 &&  "Layout does not contain specified binding");
-        
-        auto& bindingDescription = m_setLayout.m_bindings[binding];
-        
-        assert(
-            bindingDescription.descriptorCount == 1 && 
-            "Binding single descriptor info, but binding expects multiple");
-        
-        VkWriteDescriptorSet write{};
-        write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-        write.descriptorType = bindingDescription.descriptorType;
-        write.dstBinding = binding;
-        write.pBufferInfo = bufferInfo;
-        write.descriptorCount = 1;
-        
-        m_writes.push_back(write);
-        return *this;
-    }
-
-    DescriptorWriter& DescriptorWriter::writeImage(
-            uint32_t binding, VkDescriptorImageInfo* imageInfo) {
-        assert(m_setLayout.m_bindings.count(binding) == 1 && "Layout does not contain specified binding");
-        
-        auto& bindingDescription = m_setLayout.m_bindings[binding];
-        
-        assert(
-            bindingDescription.descriptorCount == 1 && 
-            "Binding single descriptor info, but binding expects multiple");
-        
-        VkWriteDescriptorSet write{};
-        write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-        write.descriptorType = bindingDescription.descriptorType;
-        write.dstBinding = binding;
-        write.pImageInfo = imageInfo;
-        write.descriptorCount = 1;
-        
-        m_writes.push_back(write);
-        return *this;
-    }
-
     bool DescriptorWriter::build(VkDescriptorSet& set) {
         bool success = m_pool.allocateDescriptorSet(m_setLayout.getDescriptorSetLayout(), set);
         if (!success) {
--- Engine/src/graphics/descriptors/descriptor_writer.hpp
+++ Engine/src/graphics/descriptors/descriptor_writer.hpp
@@ -3,18 +3,60 @@
 #include "graphics/descriptors/descriptor_set_layout.hpp"
 #include "graphics/descriptors/descriptor_pool.hpp"
 
+#include <cassert>
+#include <stdexcept>
+
 namespace PXTEngine {
     class DescriptorWriter {
     public:
         DescriptorWriter(DescriptorSetLayout& setLayout, DescriptorPool& pool);
 
-        DescriptorWriter& writeBuffer(uint32_t binding, VkDescriptorBufferInfo* bufferInfo);
-        DescriptorWriter& writeImage(uint32_t binding, VkDescriptorImageInfo* imageInfo);
+        DescriptorWriter& writeBuffer(uint32_t binding, VkDescriptorBufferInfo* bufferInfo) {
+            return write(binding, bufferInfo, 1);
+        }
+    
+        DescriptorWriter& writeBuffers(uint32_t binding, VkDescriptorBufferInfo* buffersInfo, uint32_t count) {
+            return write(binding, buffersInfo, count);
+        }
+    
+        DescriptorWriter& writeImage(uint32_t binding, VkDescriptorImageInfo* imageInfo) {
+            return write(binding, imageInfo, 1);
+        }
+    
+        DescriptorWriter& writeImages(uint32_t binding, VkDescriptorImageInfo* imagesInfo, uint32_t count) {
+            return write(binding, imagesInfo, count);
+        }
 
         bool build(VkDescriptorSet& set);
         void overwrite(VkDescriptorSet& set);
 
     private:
+        template <typename T>
+        DescriptorWriter& write(uint32_t binding, T* info, uint32_t count) {
+            assert(m_setLayout.m_bindings.count(binding) == 1 && "Layout does not contain specified binding");
+            
+            auto& bindingDescription = m_setLayout.m_bindings[binding];
+            
+            assert(bindingDescription.descriptorCount == count && "Binding descriptor info count mismatch");
+            
+            VkWriteDescriptorSet write{};
+            write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+            write.descriptorType = bindingDescription.descriptorType;
+            write.dstBinding = binding;
+            write.descriptorCount = count;
+            
+            if constexpr (std::is_same_v<T, VkDescriptorBufferInfo>) {
+                write.pBufferInfo = info;
+            } else if constexpr (std::is_same_v<T, VkDescriptorImageInfo>) {
+                write.pImageInfo = info;
+            } else {
+                static_assert(false, "Unsupported type for descriptor write");
+            }
+            
+            m_writes.push_back(write);
+            return *this;
+        }
+
         DescriptorSetLayout& m_setLayout;
         DescriptorPool& m_pool;
         std::vector<VkWriteDescriptorSet> m_writes;
