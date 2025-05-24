349c6c853eed685a794d46f42a2503d635b06781
Xhst <82611659+Xhst@users.noreply.github.com>
2025-05-24 17:43:56 +0200

    refactor: texture registry splitted in hpp and cpp

---
+++ Engine/src/graphics/resources/texture_registry.cpp
@@ -0,0 +1,72 @@
+#include "graphics/resources/texture_registry.hpp"
+
+namespace PXTEngine {
+
+	TextureRegistry::TextureRegistry(Context& context)
+		: m_context(context) {
+		m_textureDescriptorSet = VK_NULL_HANDLE;
+		m_textureDescriptorSetLayout = nullptr;
+		m_descriptorAllocator = nullptr;
+	}
+
+	void TextureRegistry::setDescriptorAllocator(Shared<DescriptorAllocatorGrowable> descriptorAllocator) {
+		m_descriptorAllocator = descriptorAllocator;
+	}
+
+	uint32_t TextureRegistry::add(const Shared<Image>& image) {
+		auto* texture = dynamic_cast<Texture2D*>(image.get());
+		if (!texture) {
+			return 0;
+		}
+
+		const auto index = static_cast<uint32_t>(m_textures.size());
+		m_textures.push_back(image);
+		m_idToIndex[image->id] = index;
+
+		return index;
+	}
+
+	uint32_t TextureRegistry::getIndex(const ResourceId& id) const {
+		auto it = m_idToIndex.find(id);
+		return it != m_idToIndex.end() ? it->second : 0;
+	}
+
+	uint32_t TextureRegistry::getTextureCount() const {
+		return static_cast<uint32_t>(m_textures.size());
+	}
+
+	std::vector<Shared<Image>> TextureRegistry::getTextures() {
+		return m_textures;
+	}
+
+	VkDescriptorSet TextureRegistry::getDescriptorSet() {
+		return m_textureDescriptorSet;
+	}
+
+	VkDescriptorSetLayout TextureRegistry::getDescriptorSetLayout() {
+		return m_textureDescriptorSetLayout->getDescriptorSetLayout();
+	}
+
+	void TextureRegistry::createDescriptorSet() {
+		m_textureDescriptorSetLayout = DescriptorSetLayout::Builder(m_context)
+			.addBinding(0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT | VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR, getTextureCount())
+			.build();
+
+		std::vector<VkDescriptorImageInfo> imageInfos;
+		for (const auto& image : m_textures) {
+			const auto texture = std::static_pointer_cast<Texture2D>(image);
+
+			VkDescriptorImageInfo imageInfo{};
+			imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+			imageInfo.imageView = texture->getImageView();
+			imageInfo.sampler = texture->getImageSampler();
+			imageInfos.push_back(imageInfo);
+		}
+
+		m_descriptorAllocator->allocate(m_textureDescriptorSetLayout->getDescriptorSetLayout(), m_textureDescriptorSet);
+
+		DescriptorWriter(m_context, *m_textureDescriptorSetLayout)
+			.writeImages(0, imageInfos.data(), static_cast<uint32_t>(imageInfos.size()))
+			.updateSet(m_textureDescriptorSet);
+	}
+}
--- Engine/src/graphics/resources/texture_registry.hpp
+++ Engine/src/graphics/resources/texture_registry.hpp
@@ -3,6 +3,7 @@
 #include "core/memory.hpp"
 #include "resources/resource.hpp"
 #include "resources/types/image.hpp"
+#include "graphics/descriptors/descriptors.hpp"
 #include "graphics/resources/texture2d.hpp"
 
 #include <vector>
@@ -12,88 +13,25 @@ namespace PXTEngine {
 
 	class TextureRegistry {
 	public:
-		TextureRegistry(Context& context) : m_context(context) {
-			m_textureDescriptorSet = VK_NULL_HANDLE;
-			m_textureDescriptorSetLayout = nullptr;
-			m_descriptorAllocator = nullptr;
-		}
+		explicit TextureRegistry(Context& context);
 
-		void setDescriptorAllocator(Shared<DescriptorAllocatorGrowable> descriptorAllocator) {
-			m_descriptorAllocator = descriptorAllocator;
-		}
+		void setDescriptorAllocator(Shared<DescriptorAllocatorGrowable> descriptorAllocator);
+		uint32_t add(const Shared<Image>& image);
 
-		uint32_t add(const Shared<Image>& image) {
-			auto* texture = dynamic_cast<Texture2D*>(image.get());
+		[[nodiscard]] uint32_t getIndex(const ResourceId& id) const;
+		[[nodiscard]] uint32_t getTextureCount() const;
+		std::vector<Shared<Image>> getTextures();
 
-			if (!texture) {
-				return 0;
-			}
+		VkDescriptorSet getDescriptorSet();
+		VkDescriptorSetLayout getDescriptorSetLayout();
 
-			// Add the resource to the list
-			const auto index = static_cast<uint32_t>(m_textures.size());
-			m_textures.push_back(image);
-
-			m_idToIndex[image->id] = index;
-
-			return index;
-		}
-
-		[[nodiscard]] uint32_t getIndex(const ResourceId& id) const {
-			auto it = m_idToIndex.find(id);
-
-			if (it != m_idToIndex.end()) {
-				return it->second;
-			}
-
-			return 0;
-		}
-
-		[[nodiscard]] uint32_t getTextureCount() const {
-			return static_cast<uint32_t>(m_textures.size());
-		}
-
-		std::vector<Shared<Image>> getTextures() {
-			return m_textures;
-		}
-
-		VkDescriptorSet getDescriptorSet() {
-			return m_textureDescriptorSet;
-		}
-
-		VkDescriptorSetLayout getDescriptorSetLayout() {
-			return m_textureDescriptorSetLayout->getDescriptorSetLayout();
-		}
-
-		void createDescriptorSet() {
-			// TEXTURE DESCRIPTOR SET
-			m_textureDescriptorSetLayout = DescriptorSetLayout::Builder(m_context)
-				.addBinding(0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT | VK_SHADER_STAGE_CLOSEST_HIT_BIT_KHR, getTextureCount())
-				.build();
-
-			std::vector<VkDescriptorImageInfo> imageInfos;
-			for (const auto& image : m_textures) {
-				const auto texture = std::static_pointer_cast<Texture2D>(image);
-
-				VkDescriptorImageInfo imageInfo{};
-				imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-				imageInfo.imageView = texture->getImageView();
-				imageInfo.sampler = texture->getImageSampler();
-				imageInfos.push_back(imageInfo);
-			}
-
-			m_descriptorAllocator->allocate(m_textureDescriptorSetLayout->getDescriptorSetLayout(), m_textureDescriptorSet);
-
-			DescriptorWriter(m_context, *m_textureDescriptorSetLayout)
-				.writeImages(0, imageInfos.data(), static_cast<uint32_t>(imageInfos.size()))
-				.updateSet(m_textureDescriptorSet);
-		}
+		void createDescriptorSet();
 
 	private:
 		std::vector<Shared<Image>> m_textures;
 		std::unordered_map<ResourceId, uint32_t> m_idToIndex;
 
 		Context& m_context;
-
 		Shared<DescriptorAllocatorGrowable> m_descriptorAllocator;
 		Shared<DescriptorSetLayout> m_textureDescriptorSetLayout;
 		VkDescriptorSet m_textureDescriptorSet;
